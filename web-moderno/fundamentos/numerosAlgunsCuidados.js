6a8d4eadf5685fb233183ff54bce19819bf7ae62
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-24 20:52:43 -0300

    infinito, IEEE e função dentro de parenteses

---
+++ web-moderno/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,5 @@
+console.log(7 / 0) // Infinito
+console.log("10" / 2) // Funciona
+console.log("Show!" * 2) // Não funciona
+console.log(0.1 + 0.7) // Especificação IEEE
+console.log((10).toFixed(2)) // Chamando a função dentro de parentes.
