912e354ae1532a82a66a55034be80a266cda21f4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-26 00:54:56 -0300

    string, number e typeof

--- fundamentos/tipagemFraca.js
+++ fundamentos/tipagemFraca.js
@@ -6,7 +6,7 @@ qualquer = 3.1516
 console.log(qualquer)
 console.log(typeof qualquer)
 
-// Evitar nome genéricos e siglas
+// Evitar nomes genéricos e siglas
 let valor = ''
 let numero = 1
 let pqp = false // Produto Químico Perigoso... Kkk
