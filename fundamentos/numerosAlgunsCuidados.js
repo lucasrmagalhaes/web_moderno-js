a5ea53d4ad2ac50c951bcf7c2c02cc1d06c45b90
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-26 01:06:39 -0300

    infinito, IEEE e String ganha!

--- fundamentos/numerosAlgunsCuidados.js
+++ fundamentos/numerosAlgunsCuidados.js
@@ -1,6 +1,6 @@
 console.log(7 / 0) // Infinito
 console.log("10" / 2) // Funciona
-console.log('3' + 2) // Concatena - String ganha!!!
+console.log('3' + 2) // Concatena - String ganha!
 console.log("Show!" * 2) // Não funciona
 console.log(0.1 + 0.7) // Especificação IEEE
 console.log((10).toFixed(2)) // Chamando a função dentro de parentes.
