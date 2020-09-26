c916e80ebe013ee4176388aec0130080fc3ac6cc
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-25 23:59:37 -0300

    objeto {}

---
+++ fundamentos/objeto.js
@@ -0,0 +1,14 @@
+// Anotação literal
+const prod1 = {} // {} - Objeto vazio
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = { // Outra maneira
+    nome: 'Camisa Polo',
+    preco: 79.90,
+}
+
+console.log(prod2)
