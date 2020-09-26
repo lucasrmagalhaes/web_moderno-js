e5c2a4e9482d6e4c292c923091be4fd3c17eb245
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-26 02:29:19 -0300

    exemplo de let - escopo

--- fundamentos/usandoLet1.js
+++ fundamentos/usandoLet1.js
@@ -2,7 +2,7 @@ var numero = 1
 
 {
     let numero = 2
-    console.log('dentro =', numero)
+    console.log('dentro =', numero) // 2
 }
 
-console.log('fora =', numero)
+console.log('fora =', numero) // 1
