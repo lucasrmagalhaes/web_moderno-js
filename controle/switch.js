21530af197f230eb974cf8a8c2a8dad0e040de0f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-28 16:57:23 -0300

    exemplo de switch

---
+++ controle/switch.js
@@ -0,0 +1,25 @@
+const imprimirResultado = function (nota) {
+    switch (Math.floor(nota)) {
+        case 10:
+        case 9:
+            console.log('Quadro de Honra')
+            break
+        case 8: case 7:
+            console.log('Aprovado')
+            break
+        case 6: case 5: case 4:
+            console.log('Recuperação')
+            break
+        case 3: case 2: case 1: case 0:
+            console.log('Reprovado')
+            break
+        default:
+            console.log('Nota Inválida')
+    }
+}
+
+imprimirResultado(10) // Quando de Honra
+imprimirResultado(8.9) // Aprovado
+imprimirResultado(6.55) // Recuperação
+imprimirResultado(2.3) // Reprovado
+imprimirResultado(-1) // Nota Inválida
