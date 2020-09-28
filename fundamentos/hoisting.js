a5d74468d09859816c7aee1eb90b8e0cd816ee83
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-28 11:41:52 -0300

    entendendo o hoisting

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,16 @@
+console.log('a = ', a) // Undefined
+var a = 2
+console.log('a = ', a) // a = 2
+
+console.log('b = ', b)
+let b = 2
+console.log('b = ', b) // Erro
+
+/*
+ * Como ficou o código acima:
+ *
+ * var a
+ * console.log('a = ', a)
+ * a = 2
+ * console.log('a = ', a) 
+ */
