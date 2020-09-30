9faf92f0ebcaee9d4c69cf62a8b67f410fcbfa4d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-30 00:32:18 -0300

    first-class object

--- funcao/cidadaoPrimeiraClasse.js
+++ funcao/cidadaoPrimeiraClasse.js
@@ -2,4 +2,35 @@
  * Higher-order function
  */
 
- 
+ // Criar de forma literal
+ function fun1() { }
+
+ // Armazenar em uma variável
+ const fun2 = function () { }
+
+ // Armazenar em um Array
+ const array = [function (a, b) { return a + b}, fun1, fun2]
+ console.log(array[0](2, 3))
+
+ // Armazenar em um atributo de objeto
+ const obj = {}
+ obj.falar = function () { return 'Opa' }
+ console.log(obj.falar())
+
+ // Passar função como parametro
+ function run(fun) {
+     fun()
+ }
+
+ run (function () { console.log('Executando...') })
+
+ // Uma função pode retornar/conter uma função
+ function soma (a, b) {
+     return function (c) {
+         console.log(a + b + c)
+     }
+ }
+
+ soma (2, 3)(4)
+ const cincoMais = soma(2, 3)
+ cincoMais(4)
