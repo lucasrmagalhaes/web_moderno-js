597a14d6e873db486474ce23e0c118fe962f26fd
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-30 17:26:24 -0300

    exemplo com e sem callback

---
+++ funcao/callback2.js
@@ -0,0 +1,23 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+// Sem Callback
+let notasBaixas1 = []
+for (let i in notas) {
+    if (notas[i] < 7) {
+        notasBaixas1.push(notas[i])
+    }
+}
+
+console.log(notasBaixas1)
+
+// Com Callback
+const notasBaixas2 = notas.filter(function (nota) {
+    return nota < 7
+})
+
+console.log(notasBaixas2)
+
+// Com Callback
+const notasMenorQue7 = nota => nota < 7
+const notasBaixas3 = notas.filter(notasMenorQue7)
+console.log(notasBaixas3)
