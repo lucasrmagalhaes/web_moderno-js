050fef305f2e0c6a40eda9422b50ec07f3ab774a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-30 17:44:12 -0300

    conceito funcao construtora

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,32 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+    // atributo privado
+    let velocidadeAtual = 0
+
+    // metodo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    // metodo publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual()) // 5
+
+const ferrari = new Carro (350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual()) // 60
+
+console.log(typeof Carro) // function
+console.log(typeof uno) // object
+console.log(typeof ferrari) // object
