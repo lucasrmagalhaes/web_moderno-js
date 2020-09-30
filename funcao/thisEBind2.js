6c85132e92b0adcfcfbda882f7578b7d79ec5640
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-30 16:03:21 -0300

    exemplo de bind e this

---
+++ funcao/thisEBind2.js
@@ -0,0 +1,11 @@
+function Pessoa() {
+    this.idade = 0
+
+    const self = this
+    setInterval(function() {
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000) // representa 1 segundo, cada segundo adiciona a idade.
+}
+
+new Pessoa
