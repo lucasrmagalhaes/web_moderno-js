7ef96c51de0ae69999b50afc3d6856b7cdc60a93
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-09-30 16:25:50 -0300

    comparações com o this

---
+++ funcao/arrowFunction3.js
@@ -0,0 +1,18 @@
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global) // true
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global) // false
+comparaComThis(obj) // true
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global) // false
+comparaComThisArrow(module.exports) // true
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj) // false
+comparaComThisArrow(module.exports) // true
