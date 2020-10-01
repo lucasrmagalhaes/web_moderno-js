6bef22877269028f6d2b0117e9d8639ab8f24611
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-01 00:11:23 -0300

    oo

---
+++ objeto/oo.js
@@ -0,0 +1,26 @@
+// Código não executável!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto.
+
+/* Principios Importantes:
+ * 1. Abstração;
+ * 2. Encapsulamento;
+ * 3. Herança (prototype); e
+ * 4. Polimorfismo.
+ */
+
+/* Abstração: Pegar um objeto da vida real e abstrair/simplificar/transformar em software. */
