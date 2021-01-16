91bb54f2c4be012cadf1ee65c041affcb8ea1226
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-16 13:57:49 -0300

    classe 1

---
+++ objeto/classe1.js
@@ -0,0 +1,33 @@
+class Lancamento {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidade = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidade += l.valor
+        })
+        return valorConsolidade
+    }
+}
+
+const salario = new Lancamento ('Salario', 45000)
+const contaDeLuz = new Lancamento('Luz', -220)
+
+const contas = new CicloFinanceiro(6, 2018)
+contas.addLancamentos(salario, contaDeLuz)
+console.log(contas.sumario()) // 44780
