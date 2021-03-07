55b032c272d12ea34eac61d40c92c6fb9f414f3b
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-03-07 20:05:31 -0300

    map

---
+++ array/map1.js
@@ -0,0 +1,16 @@
+const nums = [1, 2, 3, 4, 5]
+
+// For com propósito
+let resultado = nums.map(function(e) {
+    return e * 2
+})
+
+console.log(resultado) // [ 2, 4, 6, 8, 10 ]
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+
+console.log(resultado) // [ 'R$ 33,00', 'R$ 36,00', 'R$ 39,00', 'R$ 42,00', 'R$ 45,00' ]
