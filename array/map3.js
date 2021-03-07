6dcb40a866f0f9bafbc23ce031689ce7b133e576
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-03-07 20:18:16 -0300

    funcionamento interno do map

---
+++ array/map3.js
@@ -0,0 +1,22 @@
+Array.prototype.map2 = function(callback) {
+    const newArray = []
+    
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lápis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornando um array apenas com os preços
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado) // [ 3.45, 13.9, 41.22, 7.5 ]
