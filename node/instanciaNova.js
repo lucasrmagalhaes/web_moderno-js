17d65dc81ea23c3ffd339b37e38ad52e777d1a59
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-12-11 18:53:37 -0300

    Instância
    Instância Única vs Nova Instância

---
+++ node/instanciaNova.js
@@ -0,0 +1,9 @@
+module.exports = () => {
+    return {
+        valor: 1,
+
+        inc() {
+            this.valor++
+        }
+    }
+}
---
+++ node/instanciaUnica.js
@@ -0,0 +1,8 @@
+// node faz cache
+module.exports = {
+    valor: 1,
+    
+    inc() {
+        this.valor++
+    }
+}
