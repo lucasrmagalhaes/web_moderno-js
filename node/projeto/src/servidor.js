4c900f710abc45b98fa93bbca2ca56f2da75bd6a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-12-28 22:02:09 -0300

    API com Express 02

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,11 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancaDeDados')
 
 // .use - atende todas as URL's
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 }) // Converte para JSON
+    res.send(bancoDeDados.getProdutos()) // Converte para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+
+    res.send(produto)
 })
 
 app.listen(porta, () => {
