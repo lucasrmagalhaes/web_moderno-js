d2486a88043a4f29546cfe545598fc5757441503
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-12 19:46:49 -0300

    Trabalhando com Dados 02

---
+++ inicio/dados_02.js
@@ -0,0 +1,18 @@
+console.log(19.9 * 0.6);
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+
+console.log(precoComDesconto);
+
+let nome = "Caderno"; // String (texto)
+let categoria = "Papelaria";
+
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
+
+console.log(1 + 1); // 2
+console.log("1" + 1); // Concatenação - 11
