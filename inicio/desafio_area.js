1373fcb204b801edd3fe64cd40d039cbf0844c70
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-13 09:28:24 -0300

    Desafio Área da Circunferência

---
+++ inicio/desafio_area.js
@@ -0,0 +1,9 @@
+const PI = 3.14; 
+const raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log(`O valor da área é ${areaCirc}m2.`);
+
+areaCirc = Math.PI * raio * raio;
+
+console.log(`O valor da área é ${areaCirc}m2.`);
