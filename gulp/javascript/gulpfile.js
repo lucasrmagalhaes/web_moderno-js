46c2ae43f1d5aa20925b8f57bba81453b8c630c1
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-03-21 22:57:37 -0300

    Processando JavaScript com Gulp

---
+++ gulp/javascript/gulpfile.js
@@ -0,0 +1,26 @@
+const { series } = require('gulp')
+
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+function transformacaoJS(cb) {
+    return gulp.src('src/**/*.js')
+        .pipe(babel({
+            comments: false,
+            presets: ["@babel/preset-env"]
+        }))
+        .pipe(uglify())
+        .on('error', err => console.log(err))
+        .pipe(concat('codigo.min.js'))
+        .pipe(gulp.dest('build'))
+}
+
+function fim(cb) {
+    console.log('Fim!')
+
+    return cb()
+}
+
+exports.default = series(transformacaoJS, fim)
--- gulp/javascript/src/calculadora.js
+++ gulp/javascript/src/calculadora.js
@@ -1,25 +1,32 @@
-/*
- * Essa é uma calculadora bem legal!!!
- */
 const Calculadora = {
     _resultado: 0,
+
     get resultado() {
         return this._resultado
     },
+
     somar(a, b = 0) {
         const soma = a + b
+        
         this._resultado += soma
+        
         return this
     },
+
     potencia(a, b) {
         const potencia = a ** b
+        
         this._resultado += potencia
+        
         return this
     },
+
     zerar() {
         this._resultado = 0
+        
         return this
     },
+
     log() {
         console.log(this._resultado)
     }
