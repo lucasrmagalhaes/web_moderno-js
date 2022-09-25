12b0560fcfc21080c2708bd5d51889dcdeba1648
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-09-25 12:07:48 -0300

    Fundamentos de Gulp #02

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,10 +1,33 @@
 const gulp = require('gulp')
-const series = gulp.series
+const { series, parallel } = require('gulp')
 
-function copiar(cb) {
-    console.log('Tarefa de copiar!')
+const antes1 = cb => {
+    console.log('Tarefa Antes 1!');
+    
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa Antes 2!');
 
     return cb()
 }
 
-module.exports.default = series(copiar)
+const copiar = cb => {
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!');
+    
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim
+)
